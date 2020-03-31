5cbf28dadab08fbac172fb6351d61fcfffaa6601
Hanan795 <hawawdeh95@gmail.com>
2020-03-31 21:23:54 +0300

    upload new editting code whcich diaplay the top 4 movies on the page #8

--- src/App.js
+++ src/App.js
@@ -6,12 +6,37 @@ import Movies from './component/Movies/Movies'
 
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { 
+            isLoaded: false,
+            movies: [] 
+        }
+    }
+
+    componentDidMount() {
+        this.fe()
+    }
+
+    fe = () => {
+        fetch('https://api.themoviedb.org/3/discover/movie?api_key=6243f561bcd008ec397a81449573a5f4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+            .then((response) => {
+                return response.json();
+            })
+            .then((res) => {
+                this.setState({
+                    isLoaded: true,
+                    movies: res.results
+                })
+            });
+    }
+
     render(){
         return (
             <div className="main-container">
-        <h1 className = "main-heading">The top four Movies</h1>
-        <Movies />
-    </div>
+                <h1 className = "main-heading">The top four Movies</h1>
+                <Movies Movies = {this.state.movies.slice(0,4)}/>
+            </div>
         )
     }
 }
--- src/component/Movies/Movies.jsx
+++ src/component/Movies/Movies.jsx
@@ -8,7 +8,13 @@ import './Movies.css';
 class Movies extends Component {
     render(){
         return (
-            <Movie />
+            <div>
+              {this.props.Movies.map((Movies, i) => <li key = {i}>
+                    <h4>{Movies.title}</h4>
+                    <p>{Movies.overview}</p>
+                    <p> Rate: {Movies.vote_average}</p>
+                  </li>)}
+            </div>
         )
     }
 }
