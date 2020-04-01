aaddb2f5701a9aa70db58c309ad824f34c3fe743
rubasider <rubashka12@gmail.com>
2020-04-01 15:24:12 +0300

    search but with some issue

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Movies from './component/Movies/Movies'
+import Movieitem from './component/Movie/m'
+
 
 
 class App extends Component {
@@ -25,7 +27,9 @@ class App extends Component {
         this.setState({ input: event.target.value })
     }
     submitchange() {
-        this.setState({ target: this.state.input })
+        this.setState(state => ({
+            target: state.input
+        }))
     }
     fetchMovieInfo = () => {
         fetch('https://api.themoviedb.org/3/discover/movie?api_key=6243f561bcd008ec397a81449573a5f4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
@@ -39,18 +43,30 @@ class App extends Component {
             });
     }
 
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     if (this.nextState.target !== '')
+    //         return true;
+
+    // }
+
     render() {
+
         return ( <
-            div className = "main-container" >
-            <
-            h1 className = "main-heading" > The top four Movies < /h1> <
-            input type = "search"
-            onChange = { this.handelchange }
-            /><button onClick={this.submitchange}>search</button > <
-            Movies Movies = { this.state.movies.slice(0, 4) }
-            /> < /
-            div >
-        )
-    }
+                div className = "main-container" >
+
+                <
+                h1 className = "main-heading" > The top four Movies < /h1> <
+                input type = "search"
+                onChange = { this.handelchange }
+                /><button onClick={this.submitchange}>search</button > <
+                Movies Movies = { this.state.movies.slice(0, 4) }
+                />  {
+                (this.state.target !== '') && < Movieitem target = { this.state.target }
+                / >
+            } <
+            /
+        div >
+    )
+}
 }
 export default App;
