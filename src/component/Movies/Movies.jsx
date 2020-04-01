097333366c1ea5828a5e6af7964b3149582961b0
Hanan795 <hawawdeh95@gmail.com>
2020-04-01 13:10:12 +0300

    styling #3

--- src/component/Movies/Movies.jsx
+++ src/component/Movies/Movies.jsx
@@ -8,12 +8,8 @@ import './Movies.css';
 class Movies extends Component {
     render(){
         return (
-            <div>
-              {this.props.Movies.map((Movies, i) => <li key = {i}>
-                    <h4>{Movies.title}</h4>
-                    <p>{Movies.overview}</p>
-                    <p> Rate: {Movies.vote_average}</p>
-                  </li>)}
+            <div className = "movies-container">
+                  {this.props.Movies.map((movie, i) => <Movie key={i} movie ={movie}/>)}
             </div>
         )
     }
